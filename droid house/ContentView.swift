ab05d9a56b726158abe969cd166b7a2a3280c662
Festo Magembe <festomanolo@gmail.com>
2026-01-28 10:51:17 +0300

    Initial Commit

---
+++ droid house/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  droid house
+//
+//  Created by festomanolo on 28/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ droid house/droid_houseApp.swift
@@ -0,0 +1,17 @@
+//
+//  droid_houseApp.swift
+//  droid house
+//
+//  Created by festomanolo on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct droid_houseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
